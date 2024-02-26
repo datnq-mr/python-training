b91a1ad08ab3e21ab74ed00ac2b8d59e6f656a5b
datnq-mr <datnguyen2310@gmail.com>
2024-02-26 11:40:41 +0700

    init commit

---
+++ day1/main.py
@@ -0,0 +1,18 @@
+# How to install vscode for win 7: https://www.youtube.com/watch?v=J3sdBept4Io , tích chọn tất cả khi cài đặt ban đầu
+# How to install python for win 7: https://www.youtube.com/watch?v=tCjlrIowuDk, nhớ chọn PATH
+# Chọn File => Auto save
+# Cài thêm extension: 
+# - vs great icon: icon đẹp hơn, 
+# - python, 
+# - code runner: icon to lên, thêm chức năng chạy output
+# Ctrl + ,: cài đặt vscode
+# - Thay doi font
+# - format on save: khi nao Ctrl + s để lưu thì sẽ format lại
+# - Compact folder: thay đổi cách hiển thị khi có folder con, hiển thị kiểu cây thư mục
+# Setting => Extension => Run code configuration => tick Clear previos ouput
+# In ra biểu tượng cảm xúc, chuỗi unicode (ký tự có dấu): Window + .
+# - Setting => Extension => Run code configuration => Executor map => Edit in settings.json, thêm vào dòng python: set PYTHONIOENCODING=utf8 && 
+
+print("Hello world")
+
+print("My name is Đạt")
